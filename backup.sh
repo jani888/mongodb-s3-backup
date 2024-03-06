953e4ac73009cfb1834aa3c5055d29f5b3ab903d
János Hidvégi <jani.hidvegi@gmial.com>
2024-03-06 15:17:33 +0100

    fix: use correct syntax for aws s3 cp

--- backup.sh
+++ backup.sh
@@ -2,6 +2,8 @@ aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set region ${AWS_DEFAULT_REGION}
 
+DUMP_NAME=$(date +%Y-%m-%d-%H-%M-%S)-backup.gz
+
 mongodump --username=${MONGODB_ROOT_USER} \
           --password=${MONGODB_ROOT_PASSWORD} \
           --authenticationDatabase=admin \
@@ -9,6 +11,6 @@ mongodump --username=${MONGODB_ROOT_USER} \
           --port=${MONGODB_PORT} \
           --oplog \
           --gzip \
-          --archive=./backup.gz
+          --archive=./${DUMP_NAME}
 
-aws s3 cp ./backup.gz s3://${S3_ENDPOINT}/${S3_BUCKET_NAME}/$(date +%Y-%m-%d-%H-%M-%S)-backup.gz
+aws s3 cp ./${DUMP_NAME} s3://${S3_ENDPOINT}/${S3_BUCKET_NAME}
